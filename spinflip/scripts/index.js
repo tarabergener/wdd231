c955e23479631994c706f61a256a19ea85141dc0
tarabergener <tara.kelly211@gmail.com>
2024-07-24 22:07:54 -0400

    hamburger button

--- spinflip/scripts/index.js
+++ spinflip/scripts/index.js
@@ -1,3 +1,73 @@
+const button = document.querySelector(".hamburger-button");
+
+function hamburger() {
+
+    let nav = document.querySelector("nav");
+
+    if (nav.style.display === "flex") {
+
+        nav.style.display = "none";
+    } else {
+
+        nav.style.display = "flex";
+    }
+}
+
+const events = [
+    {
+        eventName: "COD Modern Warfare Community Tournament",
+        eventDate: "July 17, 2024",
+        eventStatus: "Virtually",
+        eventLocation: "Remote",
+        eventPrize: "Up to $180",
+    },
+    {
+        eventName: "Super Smash Bros Casual Tournament",
+        eventDate: "July 22, 2024",
+        eventStatus: "In Person",
+        eventLocation: "The Detour, 2200 Market Street, Carlsbad, CA", 
+        eventPrize: "No Prize",
+    },
+    {
+        eventName: "Spawn Camp Coaching & Skirmish",
+        eventDate: "July 23, 2024",
+        eventStatus: "In Person",
+        eventLocation: "7025 County Road 46A, Lake Mary, FL 32746",
+        eventPrize: "No Prize",
+    },
+];
+
+displayEvent(events)
+
+function displayEvent(eventInfo) {
+
+    document.querySelector(".events").innerHTML = "";
+    eventInfo.forEach(event => {
+
+        let card = document.createElement("section");
+        let name = document.createElement("h3");
+        let date = document.createElement("p");
+        let status = document.createElement("p");
+        let location = document.createElement("p");
+        let prize = document.createElement("P");
+
+        name.textContent = event.eventName;
+        date.innerHTML = `<p id="date"><strong>Date:</strong> ${event.eventDate}</p>`;
+        status.innerHTML = `<p id="status"><strong>Attend This Event:</strong> ${event.eventStatus}</p>`;
+        location.innerHTML = `<p id="location"><strong>Location:</strong> ${event.eventLocation}</p>`;
+        prize.innerHTML = `<p id="prize"><strong>Prize:</strong> ${event.eventPrize}</p>`;
+
+        card.appendChild(name);
+        card.appendChild(date);
+        card.appendChild(status);
+        card.appendChild(location);
+        card.appendChild(prize);
+
+        document.querySelector(".events").appendChild(card);
+    })
+}
+
+
 const url = `data/games.json`;
 //const cards = document.querySelector("#tile");
 
